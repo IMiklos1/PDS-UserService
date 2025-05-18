a74e6960453fc11dfe12a81358498eda2a7ae99a
IMiklos1 <istvanmicu@gmail.com>
2025-05-18 15:02:15 +0200

    Added swagger ui

---
+++ src/main/java/com/project/userservice/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.project.userservice;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Package Service API")
+                        .version("1.0")
+                        .description("Handles parcel/package management"));
+    }
+
+
+
+}
